c335f2ee93debe148dd6e707f7cb972e354919fd
Imene <34316110+imenebak@users.noreply.github.com>
2020-05-10 03:13:59 +0300

    Update image.py
    py3 adaptation

--- image.py
+++ image.py
@@ -7,7 +7,7 @@ from PIL import ImageStat
 import cv2
 
 def load_data(img_path,train = True):
-    gt_path = img_path.replace('.jpg','.h5').replace('images','ground_truth')
+    gt_path = img_path.replace('.jpg','.h5').replace('sequences','ground_truth')
     img = Image.open(img_path).convert('RGB')
     gt_file = h5py.File(gt_path)
     target = np.asarray(gt_file['density'])
@@ -37,7 +37,7 @@ def load_data(img_path,train = True):
     
     
     
-    target = cv2.resize(target,(target.shape[1]/8,target.shape[0]/8),interpolation = cv2.INTER_CUBIC)*64
+    target = cv2.resize(target,(target.shape[1]//8,target.shape[0]//8),interpolation = cv2.INTER_CUBIC)*64
     
     
     return img,target
